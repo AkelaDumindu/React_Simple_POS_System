f165e6a09d113af0837076035db1ca5059b99eeb
AkelaDumindu <ikadthilakshana@std.appsc.sab.ac.lk>
2024-06-20 13:10:50 +0530

    modified home page

--- simple_react_crud/src/components/Home.tsx
+++ simple_react_crud/src/components/Home.tsx
@@ -9,21 +9,37 @@ import Product from './Product';
 
 const Home: React.FC = () => {
     const [products, setProducts] = useState<Product[]>([]);
+    const [productCount, setProductCount] = useState<number>();
+    const [customerCount, setCustomerCount] = useState<number>();
+    const [orderCount, setOrderCount] = useState<number>();
 
     useEffect(() => {
         findAllProducts();
+
+        findAllCount();
     }, []); // Adding an empty dependency array to run once
 
     const findAllProducts = async () => {
-        try {
+        
             const response = await axios.get('http://localhost:3000/api/v1/products/find-all-min');
             // const response = await axios.get('http://localhost:3000/api/v1/products/find-all-product?searchText=&page=1&size=10');
-            setProducts(response.data.data);
-        } catch (error) {
-            console.error('Error fetching products:', error);
-        }
+            setProducts(response.data);
+       
     }
 
+    const findAllCount = async () => {
+        
+        const responseProduct = await axios.get('http://localhost:3000/api/v1/products/find-all-count');
+        // const response = await axios.get('http://localhost:3000/api/v1/products/find-all-product?searchText=&page=1&size=10');
+        setProductCount(responseProduct.data);
+
+        const responseCustomer = await axios.get('http://localhost:3000/api/v1/customers/find-all-count');
+        setCustomerCount(responseCustomer.data);
+
+        const responseOrder = await axios.get('http://localhost:3000/api/v1/orders/find-all-count');
+        setOrderCount(responseOrder.data);
+   
+}
     const maxWidthStyle: React.CSSProperties = {
         maxWidth: '540px'
     }
@@ -37,7 +53,7 @@ const Home: React.FC = () => {
                             thumbnail={'https://img.freepik.com/free-photo/business-partners-shaking-hands-agreement_53876-25164.jpg?t=st=1717664384~exp=1717667984~hmac=f46a6c170152f71e5646743a4152a3399d7b7c494c403b2f90c7181d877b1df1&w=360'} 
                             title={'Customers'} 
                             description={'This is a wider card with.'}
-                            value={250}
+                            value={customerCount}
                             key={1}                        
                         />
                     </div>
@@ -46,7 +62,7 @@ const Home: React.FC = () => {
                             thumbnail={'https://img.freepik.com/free-photo/cosmetic-male-beauty-products-with-display_23-2150435210.jpg?t=st=1717664446~exp=1717668046~hmac=b1870db4cd226d83b41e4557d4289229b4c4889c2f622307c707982c1ede0d1a&w=360'} 
                             title={'Products'} 
                             description={'This is a wider card with.'}
-                            value={250}
+                            value={productCount}
                             key={2}
                         />
                     </div>
@@ -55,7 +71,7 @@ const Home: React.FC = () => {
                             thumbnail={'https://img.freepik.com/free-vector/order-now-banner_52683-48722.jpg?t=st=1717664515~exp=1717668115~hmac=41ed000855650b5cd82710f652c72707a117a5942c47a4129076fa31e2f02f25&w=740'} 
                             title={'Order'} 
                             description={'This is a wider card with.'} 
-                            value={250}
+                            value={orderCount}
                             key={3}
                         />
                     </div>
